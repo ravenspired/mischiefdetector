b0d1c740e77162456a28eb83339dfb9b40d5ece1
ravenspired <53878857+ravenspired@users.noreply.github.com>
2020-01-15 12:44:05 -0500

    updated (un)installer files
    @@ -1 +1,7 @@
    #This script installs files to your computer
    #This script installs files to your computer
    
    echo "Installation is now in progress."
    
    mkdir ~/mischief_detector
    touch ~/mischief_detector/mischief.log

--- installer/installme.sh
+++ installer/installme.sh
@@ -1,1 +1,7 @@
 #This script installs files to your computer
+
+echo "Installation is now in progress."
+
+
+mkdir ~/mischief_detector
+touch ~/mischief_detector/mischief.log
--- installer/uninstallme.sh
+++ installer/uninstallme.sh
@@ -1,1 +1,6 @@
 #This script uninstalls stuff from your computer
+
+
+echo "Uninstalling mischief detector. Please wait."
+
+rm -rf ~/mischief_detector
