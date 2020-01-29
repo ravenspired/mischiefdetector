8ab2db4f63e451321c533d057c4996471df88587
c47s <36133719+c47s@users.noreply.github.com>
2020-01-29 12:14:40 -0500

    Begin Classifier
    Mostly worked on getting packages to function

---
+++ ai/imageclassifier.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras
+from keras.layers import Dense, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.models import Sequential
+
+model = keras.Sequential()
