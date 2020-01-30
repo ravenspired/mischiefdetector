d36ae51414b59a7237353d97d91ff5bb97ce8337
c47s <36133719+c47s@users.noreply.github.com>
2020-01-30 17:48:40 -0500

    Working on image preprocessing

--- ai/imageclassifier.py
+++ ai/imageclassifier.py
@@ -6,26 +6,49 @@ from keras.layers import Conv2D, MaxPooling2D
 from keras.models import Sequential
 from keras.preprocessing.image import ImageDataGenerator
 
-model = Sequential([
+#model = Sequential([
+#
+#])
 
-])
-
-intakeDatagenConfiguration = ImageDataGenerator(
+# Parameters for data generators which directly read
+# in the images and augment each individually, and thus
+# don't need to analyze the image set as a whole.
+intakeDatagenConfiguration = dict(
     zoom_range=0.2,
-    brightness_range=0.3,
+    brightness_range=[-0.3, 0.3],
     channel_shift_range=0.3,
     rescale=1./255
 )
 
-normalizDatagenConfiguration = ImageDataGenerator(
+# Parameters for data generators which normalize the images,
+# thus requiring a look at the whole dataset, which is given
+# to them by the intake datagens, as seen above.
+normalizDatagenConfiguration = dict(
     featurewise_center=True,
     featurewise_std_normalization=True
 )
 
+# Creating the data generators from the configurations above.
 trainingDatagenIntake = ImageDataGenerator(**intakeDatagenConfiguration)
 testingDatagenIntake  = ImageDataGenerator(**intakeDatagenConfiguration)
 trainingDatagenNorm = ImageDataGenerator(**normalizDatagenConfiguration)
 testingDatagenNorm  = ImageDataGenerator(**normalizDatagenConfiguration)
 
-trainingDatagenIntake.fit()
-testingDatagenNorm.fit()
+# Tell the intake data generators to take images
+# from the proper folders
+# TODO: 
+testingDatagenIntake.flow_from_directory(
+    "sorted_data/",
+    target_size=(256,144),
+    class_mode="binary",
+    save_to_dir="augmented_data"
+)
+#trainingDatagenIntake
+
+# Give the normalization data generators a look at the data
+# coming out of the intake data generators, so that they can
+# adjust (normalize) the data properly.
+# TODO: The training and testing data are normalized separately. Do we really want this?
+#trainingDatagenNorm.fit(trainingDatagenIntake)
+#testingDatagenNorm.fit(testingDatagenIntake)
+
