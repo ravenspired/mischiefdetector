f900cfa7fca62a4132521c8375ce663b1fe738fe
ravenspired <53878857+ravenspired@users.noreply.github.com>
2020-01-30 10:43:21 -0500

    Update server.py
    it works now!!!!!1!

--- classifier/server/server.py
+++ classifier/server/server.py
@@ -1,21 +1,66 @@
-#this script sends email to web service
+# Python code to illustrate Sending mail with attachments 
+# from your Gmail account 
 
-import smtplib, ssl
+# libraries to be imported 
+import smtplib 
+from email.mime.multipart import MIMEMultipart 
+from email.mime.text import MIMEText 
+from email.mime.base import MIMEBase 
+from email import encoders 
 
-port = 587  # For starttls
-smtp_server = "smtp.gmail.com"
-sender_email = "mischiefservice@gmail.com"
-receiver_email = "mischiefservice@gmail.com"
-password = "scifairproj!"
-message = """\
-Subject: Violation
+fromaddr = "mischiefservice@gmail.com"
+toaddr = "mischiefservice@gmail.com"
 
-user 'avoronov2022' has violated the TOS."""
+# instance of MIMEMultipart 
+msg = MIMEMultipart() 
 
-context = ssl.create_default_context()
-with smtplib.SMTP(smtp_server, port) as server:
-    server.ehlo()  # Can be omitted
-    server.starttls(context=context)
-    server.ehlo()  # Can be omitted
-    server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, message)
+# storing the senders email address 
+msg['From'] = fromaddr 
+
+# storing the receivers email address 
+msg['To'] = toaddr 
+
+# storing the subject 
+msg['Subject'] = "Violation"
+
+# string to store the body of the mail 
+body = "user avoronov2022 is being mischievous"
+
+# attach the body with the msg instance 
+msg.attach(MIMEText(body, 'plain')) 
+
+# open the file to be sent 
+filename = "violation.png"
+attachment = open("/Users/avoronov2022/mischief_detector/temp_scrnsht/2020-01-30_10_33_54_762429.png", "rb") 
+
+# instance of MIMEBase and named as p 
+p = MIMEBase('application', 'octet-stream') 
+
+# To change the payload into encoded form 
+p.set_payload((attachment).read()) 
+
+# encode into base64 
+encoders.encode_base64(p) 
+
+p.add_header('Content-Disposition', "attachment; filename= %s" % filename) 
+
+# attach the instance 'p' to instance 'msg' 
+msg.attach(p) 
+
+# creates SMTP session 
+s = smtplib.SMTP('smtp.gmail.com', 587) 
+
+# start TLS for security 
+s.starttls() 
+
+# Authentication 
+s.login(fromaddr, "scifairproj!") 
+
+# Converts the Multipart msg into a string 
+text = msg.as_string() 
+
+# sending the mail 
+s.sendmail(fromaddr, toaddr, text) 
+
+# terminating the session 
+s.quit() 
