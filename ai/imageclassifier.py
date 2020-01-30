80e5966b1b4a59799c81db0538633f3842cf05d9
c47s <36133719+c47s@users.noreply.github.com>
2020-01-29 22:29:00 -0500

    Update imageclassifier.py

--- ai/imageclassifier.py
+++ ai/imageclassifier.py
@@ -4,5 +4,13 @@ from tensorflow import keras
 from keras.layers import Dense, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras.models import Sequential
+from keras.preprocessing.image import ImageDataGenerator
 
-model = keras.Sequential()
+model = Sequential([
+
+])
+
+trainingDatagen = ImageDataGenerator(
+    featurewise_std_normalization=true,
+    
+)
