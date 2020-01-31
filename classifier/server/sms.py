75b0af84075651074ebc9af87f1a380c324573a9
ravenspired <53878857+ravenspired@users.noreply.github.com>
2020-01-31 11:17:02 -0500

    Update sms.py

--- classifier/server/sms.py
+++ classifier/server/sms.py
@@ -1,10 +1,66 @@
-from googlevoice import Voice
-from googlevoice.util import input
+# Python code to illustrate Sending mail with attachments 
+# from your Gmail account 
 
-voice = Voice()
-voice.login()
+# libraries to be imported 
+import smtplib 
+from email.mime.multipart import MIMEMultipart 
+from email.mime.text import MIMEText 
+from email.mime.base import MIMEBase 
+from email import encoders 
 
-phoneNumber = input('Number to send message to: ')
-text = input('Message text: ')
+fromaddr = "mischiefservice@gmail.com"
+toaddr = "9787283665@vtext.com"
 
-voice.send_sms(phoneNumber, text)
+# instance of MIMEMultipart 
+msg = MIMEMultipart() 
+
+# storing the senders email address 
+msg['From'] = fromaddr 
+
+# storing the receivers email address 
+msg['To'] = toaddr 
+
+# storing the subject 
+msg['Subject'] = "Violation"
+
+# string to store the body of the mail 
+body = "user avoronov2022 is being mischievous"
+
+# attach the body with the msg instance 
+msg.attach(MIMEText(body, 'plain')) 
+
+# open the file to be sent 
+filename = "violation.png"
+attachment = open("/Users/avoronov2022/mischief_detector/temp_scrnsht/2020-01-30_10_33_54_762429.png", "rb") 
+
+# instance of MIMEBase and named as p 
+p = MIMEBase('application', 'octet-stream') 
+
+# To change the payload into encoded form 
+p.set_payload((attachment).read()) 
+
+# encode into base64 
+encoders.encode_base64(p) 
+
+p.add_header('Content-Disposition', "attachment; filename= %s" % filename) 
+
+# attach the instance 'p' to instance 'msg' 
+msg.attach(p) 
+
+# creates SMTP session 
+s = smtplib.SMTP('smtp.gmail.com', 587) 
+
+# start TLS for security 
+s.starttls() 
+
+# Authentication 
+s.login(fromaddr, "scifairproj!") 
+
+# Converts the Multipart msg into a string 
+text = msg.as_string() 
+
+# sending the mail 
+s.sendmail(fromaddr, toaddr, text) 
+
+# terminating the session 
+s.quit() 
