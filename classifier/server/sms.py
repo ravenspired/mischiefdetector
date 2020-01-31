9511322cffc5240107350f5381cc937044290f30
ravenspired <53878857+ravenspired@users.noreply.github.com>
2020-01-31 13:44:49 -0500

    Update sms.py

--- classifier/server/sms.py
+++ classifier/server/sms.py
@@ -10,8 +10,60 @@ from email import encoders
 
 fromaddr = "mischiefservice@gmail.com"
 toaddr = "9787283665@vtext.com"
+toaddr2 = "‪7742735730‬@vtext.com"
 
-# instance of MIMEMultipart 
+
+
+def sendMassage():
+	# instance of MIMEMultipart 
+
+
+	# storing the subject 
+	msg['Subject'] = "Violation"
+
+	# string to store the body of the mail 
+	body = "user avoronov2022 is being mischievous. If you recieved this as a text message, the attachment was deleted."
+
+	# attach the body with the msg instance 
+	msg.attach(MIMEText(body, 'plain')) 
+
+	# open the file to be sent 
+	# filename = "violation.png"
+	# attachment = open("/Users/avoronov2022/mischief_detector/temp_scrnsht/2020-01-30_10_33_54_762429.png", "rb") 
+
+	# instance of MIMEBase and named as p 
+	p = MIMEBase('application', 'octet-stream') 
+
+	# To change the payload into encoded form 
+	# p.set_payload((attachment).read()) 
+
+	# encode into base64 
+	# encoders.encode_base64(p) 
+
+	# p.add_header('Content-Disposition', "attachment; filename= %s" % filename) 
+
+	# attach the instance 'p' to instance 'msg' 
+	# msg.attach(p) 
+
+	# creates SMTP session 
+	s = smtplib.SMTP('smtp.gmail.com', 587) 
+
+	# start TLS for security 
+	s.starttls() 
+
+	# Authentication 
+	s.login(fromaddr, "scifairproj!") 
+
+	# Converts the Multipart msg into a string 
+	text = msg.as_string() 
+
+	# sending the mail 
+	s.sendmail(fromaddr, toaddr, text) 
+
+	# terminating the session 
+	s.quit() 
+
+
 msg = MIMEMultipart() 
 
 # storing the senders email address 
@@ -20,47 +72,6 @@ msg['From'] = fromaddr
 # storing the receivers email address 
 msg['To'] = toaddr 
 
-# storing the subject 
-msg['Subject'] = "Violation"
-
-# string to store the body of the mail 
-body = "user avoronov2022 is being mischievous"
-
-# attach the body with the msg instance 
-msg.attach(MIMEText(body, 'plain')) 
-
-# open the file to be sent 
-filename = "violation.png"
-attachment = open("/Users/avoronov2022/mischief_detector/temp_scrnsht/2020-01-30_10_33_54_762429.png", "rb") 
-
-# instance of MIMEBase and named as p 
-p = MIMEBase('application', 'octet-stream') 
-
-# To change the payload into encoded form 
-p.set_payload((attachment).read()) 
-
-# encode into base64 
-encoders.encode_base64(p) 
-
-p.add_header('Content-Disposition', "attachment; filename= %s" % filename) 
-
-# attach the instance 'p' to instance 'msg' 
-msg.attach(p) 
-
-# creates SMTP session 
-s = smtplib.SMTP('smtp.gmail.com', 587) 
-
-# start TLS for security 
-s.starttls() 
-
-# Authentication 
-s.login(fromaddr, "scifairproj!") 
-
-# Converts the Multipart msg into a string 
-text = msg.as_string() 
-
-# sending the mail 
-s.sendmail(fromaddr, toaddr, text) 
-
-# terminating the session 
-s.quit() 
+sendMassage()
+msg['To'] = toaddr2
+sendMassage()
