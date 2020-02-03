7b4e782f06050b4cd3dc26a6269339705538d904
c47s <36133719+c47s@users.noreply.github.com>
2020-02-02 22:00:48 -0500

    Attempt to Add Image Zooming to PreProc
    Anaconda really doesn't want to install opencv. Putting the issue on hold for tonight.

--- ai/imageclassifier.py
+++ ai/imageclassifier.py
@@ -1,10 +1,49 @@
 import tensorflow as tf
 import numpy as np
+import cv2
 from tensorflow import keras
 from keras.layers import Dense, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras.models import Sequential
 from keras.preprocessing.image import ImageDataGenerator
+
+# Borrowed From StackOverflow, Page Archived at:
+# https://web.archive.org/web/20200203015002/http://stackoverflow.com/questions/37119071/scipy-rotate-and-zoom-an-image-without-changing-its-dimensions
+def cv2_clipped_zoom(img, zoom_factor):
+    """
+    Center zoom in/out of the given image and returning an enlarged/shrinked view of 
+    the image without changing dimensions
+    Args:
+        img : Image array
+        zoom_factor : amount of zoom as a ratio (0 to Inf)
+    """
+    height, width = img.shape[:2] # It's also the final desired shape
+    new_height, new_width = int(height * zoom_factor), int(width * zoom_factor)
+
+    ### Crop only the part that will remain in the result (more efficient)
+    # Centered bbox of the final desired size in resized (larger/smaller) image coordinates
+    y1, x1 = max(0, new_height - height) // 2, max(0, new_width - width) // 2
+    y2, x2 = y1 + height, x1 + width
+    bbox = np.array([y1,x1,y2,x2])
+    # Map back to original image coordinates
+    bbox = (bbox / zoom_factor).astype(np.int)
+    y1, x1, y2, x2 = bbox
+    cropped_img = img[y1:y2, x1:x2]
+
+    # Handle padding when downscaling
+    resize_height, resize_width = min(new_height, height), min(new_width, width)
+    pad_height1, pad_width1 = (height - resize_height) // 2, (width - resize_width) //2
+    pad_height2, pad_width2 = (height - resize_height) - pad_height1, (width - resize_width) - pad_width1
+    pad_spec = [(pad_height1, pad_height2), (pad_width1, pad_width2)] + [(0,0)] * (img.ndim - 2)
+
+    result = cv2.resize(cropped_img, (resize_width, resize_height))
+    result = np.pad(result, pad_spec, mode='constant')
+    assert result.shape[0] == height and result.shape[1] == width
+    return result
+
+# Apply cv2_clipped_zoom randomly to an image
+def uniform_rand_zoom(image):
+    return cv2_clipped_zoom(image, ((np.random.random() * 2) + 1))
 
 #model = Sequential([
 #
@@ -14,12 +53,12 @@ from keras.preprocessing.image import ImageDataGenerator
 # in the images and augment each individually, and thus
 # don't need to analyze the image set as a whole.
 intakeDatagenConfiguration = dict(
-    zoom_range=(0.6,1),
     channel_shift_range=60,
-    rescale=1./255
+    rescale=1./255,
+    preprocessing_function=uniform_rand_zoom
 )
 intakeDatagenFlowConfig = dict(
-    "sorted_data/monjan13",
+    directory="sorted_data/monjan13",
     target_size=(144,256),
     class_mode="binary",
     save_prefix="Intake",
