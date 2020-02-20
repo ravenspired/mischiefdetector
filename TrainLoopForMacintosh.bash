f2a59707187e84b97244a1b3da8c5fb1c0826960
c47s <36133719+c47s@users.noreply.github.com>
2020-02-19 20:32:57 -0500

    Create TrainLoopForMacintosh.bash
    CoLab keeps timing out, I'm just going to use my own mac

---
+++ TrainLoopForMacintosh.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# ( I've modified it to work on my mac )
+# TODO: Actually modify it
+
+cd ~/Documents/GitHub\ Desktop/mischiefdetector
+/root/miniconda3/bin/conda activate MischiefDetectorpython
+until read -t 3 -n 1; do
+    echo '** SYNCING **'
+    git add -A
+    git commit -m 'Automated Training Commit'
+    git pull
+    git push
+
+    echo '** TRAINING **'
+    python ai/trainclassifier.py
+
+    echo '** NEXT ROUND OF TRAINING **'
+    echo '** PRESS ANY KEY TO CANCEL **'
+done
+
+
+echo '** FINAL SYNC **'
+git add -A
+git commit -m 'Final Automated Training Commit of Session'
+git pull
+git push
