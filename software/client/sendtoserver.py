5fe0d966d0bca5c042a0ff17acbb0f1ffd437c61
ravenspired <53878857+ravenspired@users.noreply.github.com>
2020-12-06 16:17:15 -0500

    Create sendtoserver.py
    created secure client image sending program. next need to make the client and server gmail accounts.

---
+++ software/client/sendtoserver.py
@@ -0,0 +1,67 @@
+#this script sends screenshot as an attachment to the server
+
+import smtplib 
+from email.mime.multipart import MIMEMultipart 
+from email.mime.text import MIMEText 
+from email.mime.base import MIMEBase 
+from email import encoders
+import os.path
+import datetime
+homedir = os.path.expanduser("~") 
+
+fromaddr = "mischiefserviceclient@gmail.com"
+toaddr = "mischiefserviceservice@gmail.com"
+
+# instance of MIMEMultipart 
+msg = MIMEMultipart() 
+
+# storing the senders email address 
+msg['From'] = fromaddr 
+
+# storing the receivers email address 
+msg['To'] = toaddr 
+
+# storing the subject 
+msg['Subject'] = datetime.datetime.now()
+
+# string to store the body of the mail 
+body = "This email was automatically generated and sent to the server."
+
+# attach the body with the msg instance 
+msg.attach(MIMEText(body, 'plain')) 
+
+# open the file to be sent 
+filename = "untested.png"
+attachment = open(homedir+"/mischief_detector/temp_scrnsht/2020-01-30_10_33_54_762429.png", "rb") 
+
+# instance of MIMEBase and named as p 
+p = MIMEBase('application', 'octet-stream') 
+
+# To change the payload into encoded form 
+p.set_payload((attachment).read()) 
+
+# encode into base64 
+encoders.encode_base64(p) 
+
+p.add_header('Content-Disposition', "attachment; filename= %s" % filename) 
+
+# attach the instance 'p' to instance 'msg' 
+msg.attach(p) 
+
+# creates SMTP session 
+s = smtplib.SMTP('smtp.gmail.com', 587) 
+
+# start TLS for security 
+s.starttls() 
+
+# Authentication 
+s.login(fromaddr, "scifairproj!") 
+
+# Converts the Multipart msg into a string 
+text = msg.as_string() 
+
+# sending the mail 
+s.sendmail(fromaddr, toaddr, text) 
+
+# terminating the session 
+s.quit() 
