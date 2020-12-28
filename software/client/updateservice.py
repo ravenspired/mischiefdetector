a6ca1cdfc31660cbefc3ee03ed4df392630381fc
Anton Voronov <53878857+ravenspired@users.noreply.github.com>
2020-12-28 13:16:12 -0500

    edited update service

--- software/client/updateservice.py
+++ software/client/updateservice.py
@@ -1,8 +1,12 @@
 #update file to minimize cloning the repo over and over onto the computers.
 
+#At the moment, the update service will never receive updates. If this is a problem, please open an issue on GitHub.
 
 
 import urllib.request, filecmp, sys, os, time
+
+force_update = False #change to true to force an update
+files_to_update = 3 #deprecated, will be removed
 
 
 #Download check for updates file
@@ -13,17 +17,33 @@ urllib.request.urlretrieve ("https://raw.githubusercontent.com/ravenspired/misch
 #Compare it with existing file
 files = filecmp.cmp('originalupdate.txt', 'sampleupdate.txt', shallow=False)
 
+if force_update == False:
+	files = False
 
 
 #Update if neccesary
 if files == False:
     print("Updating Software...")
     time.sleep(1)
-    urllib.request.urlretrieve ("http://host.com/updatedsoftware.script", "update.py")#saves the update as a python file
+    print("Downloading updated files...")
+
+
+    urllib.request.urlretrieve ("https://raw.githubusercontent.com/ravenspired/mischiefdetector/master/software/client/imagecompressor.py", "imagecompressor_updated.py")
+    urllib.request.urlretrieve ("https://raw.githubusercontent.com/ravenspired/mischiefdetector/master/software/client/sendtoserver.py", "sendtoserver_updated.py")
+    urllib.request.urlretrieve ("https://github.com/ravenspired/mischiefdetector/blob/master/software/client/service.py", "service_updated.py")#saves the update as a python file
+    print("unpacking and replacing old files with new...")
     os.remove("originalupdate.txt")
     os.rename("sampleupdate.txt", "originalupdate.txt")
+    os.remove("imagecompressor.py")
+    os.remove("sendtoserver.py")
     os.remove("service.py")
-    os.rename("update.py", "service.py")
+    os.rename("imagecompressor_updated.py", "imagecompressor.py")
+    os.rename("sendtoserver_updated.py", "sendtoserver.py")
+    os.rename("service_updated.py", "service.py")
+
+    print("Updated successfully.")
+
+    
 
 
 else:
@@ -31,3 +51,6 @@ else:
     os.remove("sampleupdate.txt")
 
 import service#run program as update check passed
+
+
+#
