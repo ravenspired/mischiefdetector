f09e3ab0941902b9bf19026d326b9239641e92bd
c47s <36133719+c47s@users.noreply.github.com>
2020-12-30 17:23:45 -0500

    Finish mchfDet example use program

--- example_classifier_use.py
+++ example_classifier_use.py
@@ -3,10 +3,12 @@ import ai.classifier as mischief
 from pandas import DataFrame
 import numpy as np
 
+SCREENCAP_DIR = "screencaps/"
+
 os.system("""  # This shell script takes screenshots of monitors
                # until it runs out of monitors.
   screenNum=1
-  while screencapture -xD $screenNum screencaps/subfolder/cap${screenNum}.png
+  while screencapture -xD $screenNum """ + SCREENCAP_DIR + """subfolder/cap${screenNum}.png
   do
     screenNum=$((screenNum+1))
   done
@@ -20,7 +22,12 @@ os.system("""  # This shell script takes screenshots of monitors
 
 mischief.load_from_checkpt()
 mischief.compile_network()
-results = mischief.predict_in_directory("screencaps/")
+results = mischief.predict_in_directory(SCREENCAP_DIR)
 
-for a, b in results.items():
-    print(a)
+for index, image_info in results.iterrows():
+    outstring = "The image " + SCREENCAP_DIR + image_info['file'] + " is "
+    if image_info['class'] == 0:
+        outstring = outstring + "not "
+    print(outstring + "mischievous.")
+
+os.system("rm -f " + SCREENCAP_DIR + "subfolder/cap*.png")
