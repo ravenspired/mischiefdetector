c89eb2fa3dbbb5abd50372a7d77c2ab0c5e9dda5
c47s <36133719+c47s@users.noreply.github.com>
2021-01-28 17:37:11 -0500

    Add random delay

--- software/client/service.py
+++ software/client/service.py
@@ -1,5 +1,7 @@
 #This is the main program that facilitates running the others. In other words, the logic loop.
 
+from time import sleep
+from random import randint
 
 import updateservice
 import imagecapture
@@ -7,6 +9,8 @@ import ai
 import sendtoserver
 
 SCREENCAP_DIR = "temp/"
+MIN_DELAY_MINUTES = 2
+MAX_DELAY_MINUTES = 5
 
 #Program loop
 while True:
@@ -14,6 +18,6 @@ while True:
     detector.test_screenshot()
     sendtoserver.report_any_mischief()
 
+    sleep(randint(MIN_DELAY_MINUTES*60, MAX_DELAY_MINUTES*60))
 
-
-#program should be run as root to avoid students killing it
+# This program should be run as root to avoid students killing it
